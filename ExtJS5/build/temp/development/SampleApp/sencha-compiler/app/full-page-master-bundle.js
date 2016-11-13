9fb5135e91982bb5e84e6cbe279be0d5a0edc449
Ramesh Manne <rmanne@Rameshs-Macbook-Pro.local>
2016-11-13 14:01:22 -0800

    ExtJS5 workspace with sample application

---
+++ ExtJS5/build/temp/development/SampleApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/rmanne/work/code/personal_git/ExtJS/ExtJS5/resource/SampleApp/app.js
---
+++ ExtJS5/resource/SampleApp/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('SampleApp.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'SampleApp'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
